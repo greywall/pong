d44a4bcf992de9cc6f2c3fdbc5388b2b1b2fb8c0
Tal G <talwindergrewal01@gmail.com>
2019-07-18 16:25:27 -0700

    Add Ball

---
+++ src/partials/Ball.js
@@ -0,0 +1,21 @@
+import { SVG_NS } from "../settings";
+
+export default class Ball {
+  constructor(radius, boardWidth, boardHeight, color = "white") {
+    this.radius = radius;
+    this.boardWidth = boardWidth;
+    this.boardHeight = boardHeight;
+    this.direction = 1;
+    this.color = color;
+  }
+
+  render(svg) {
+    let circle = document.createElementNS(SVG_NS, "circle");
+    circle.setAttributeNS(null, "r", this.radius);
+    circle.setAttributeNS(null, "cx", this.boardWidth / 2);
+    circle.setAttributeNS(null, "cy", this.boardHeight / 2);
+    circle.setAttributeNS(null, "fill", this.color);
+
+    svg.appendChild(circle); //append passes svg to parent.
+  }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,9 +1,9 @@
 import Board from "./Board";
 import Paddle from "./Paddle";
-import { SVG_NS, KEYS } from "../settings";
+import Ball from "./Ball";
+import { SVG_NS, KEYS, PaddleOptions } from "../settings";
 
 export default class Game {
-
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
@@ -11,50 +11,49 @@ export default class Game {
 
     this.gameElement = document.getElementById(this.element);
     this.board = new Board(this.width, this.height);
-  
-      this.paddleWidth = 8;
-      this.paddleHeight = 56;
-      this.boardGap = 10;
-    
 
-      this.player1 = new Paddle(
-        this.height, //board height
-        this.paddleWidth,
-        this.paddleHeight,
-        this.boardGap,
-        ((this.height - this.paddleHeight) / 2),
-        'red',
-        KEYS.up,
-        KEYS.down,
-      );
-      
-      this.player2 = new Paddle(
-        this.height, //board height
-        this.paddleWidth,
-        this.paddleHeight,
-        this.width - (this.boardGap + this.paddleWidth), 
-        ((this.height - this.paddleHeight) /2),
-        'blue',
-        KEYS.a,
-        KEYS.z,
-      );
-    
+    // this.paddleWidth = 8;
+    // this.paddleHeight = 56;
+    // this.boardGap = 10;
+
+    this.player1 = new Paddle(
+      this.height, //board height
+      PaddleOptions.paddleWidth,
+      PaddleOptions.paddleHeight,
+      PaddleOptions.boardGap,
+      (this.height - PaddleOptions.paddleHeight) / 2,
+      "red",
+      KEYS.up,
+      KEYS.down
+    );
+
+    this.player2 = new Paddle(
+      this.height, //board height
+      PaddleOptions.paddleWidth,
+      PaddleOptions.paddleHeight,
+      this.width - (PaddleOptions.boardGap + PaddleOptions.paddleWidth),
+      (this.height - PaddleOptions.paddleHeight) / 2,
+      "blue",
+      KEYS.a,
+      KEYS.z
+    );
+
+    this.ball = new Ball(8, this.width, this.height, "black");
 
     // this.player1 = new Paddle(this.height, 8, 56, 10,100, '#f69d3c' );
     // this.player2 = new Paddle(this.height, 8, 56, 494,100,'#3f87a6');
-
   } //end of constructor
 
   render() {
-   this.gameElement.innerHTML = ''; //clear the html before appending to fix the render bug
+    this.gameElement.innerHTML = ""; //clear the html before appending to fix the render bug
     let svg = document.createElementNS(SVG_NS, "svg");
     svg.setAttributeNS(null, "width", this.width);
     svg.setAttributeNS(null, "height", this.height);
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-    this.gameElement.appendChild(svg); 
+    this.gameElement.appendChild(svg);
     this.board.render(svg);
     this.player1.render(svg);
     this.player2.render(svg);
-    
-}
+    this.ball.render(svg);
+  }
 }
